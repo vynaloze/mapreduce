a02d5ca63c4ed21d3cabfbd8d133f32f24462400
Piotr Pawluk <vynaloze@gmail.com>
2021-10-28 20:00:05 +0200

    draft of internal API (worker.proto)

--- proto_compile.sh
+++ proto_compile.sh
@@ -2,9 +2,8 @@
 
 protoc --go_out=. --go_opt=paths=source_relative \
     --go-grpc_out=. --go-grpc_opt=paths=source_relative \
-    engine/api/registry.proto
+    api/mapreduce.proto
 
 protoc --go_out=. --go_opt=paths=source_relative \
     --go-grpc_out=. --go-grpc_opt=paths=source_relative \
-    api/mapreduce.proto
-
+    engine/api/registry.proto engine/api/worker.proto
